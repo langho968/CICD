e68ee7c9026bfe3500255900df57ba7ab3b1f154
langho968 <langho968@gmail.com>
2022-05-09 18:11:13 +0900

    1

--- src/main/java/com/example/cicdtest/CicdTestApplication.java
+++ src/main/java/com/example/cicdtest/CicdTestApplication.java
@@ -2,20 +2,12 @@ package com.example.cicdtest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class CicdTestApplication {
 
-    public static final String APPLICATION_LOCATIONS = "spring.config.location="
-            + "classpath:application.properties,"
-            + "classpath:appspec.yml";
-
-    // S3
-// application.properties와 aws.yml 두개의 파일 모두를 설정 파일로 읽어서 사용
     public static void main(String[] args) {
-        new SpringApplicationBuilder(CicdTestApplication.class)
-                .properties(APPLICATION_LOCATIONS)
-                .run(args);
+        SpringApplication.run(CicdTestApplication.class, args);
     }
+
 }
