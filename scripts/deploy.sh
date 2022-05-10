3fbf8794dbd9ebf21999727acd7df42fcbca513d
langho968 <langho968@gmail.com>
2022-05-10 22:20:40 +0900

    대량수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,39 +1,165 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-REPOSITORY=/home/ec2-user/app/step2
-PROJECT_NAME=mulmul
+# profile.sh
+# 미사용 중인 profile을 잡는다.
+
+function find_idle_profile()
+{
+    # curl 결과로 연결할 서비스 결정
+    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
+
+    if [ ${RESPONSE_CODE} -ge 400 ] # 400 보다 크면 (즉, 40x/50x 에러 모두 포함)
+    then
+        CURRENT_PROFILE=real2
+    else
+        CURRENT_PROFILE=$(curl -s http://localhost/profile)
+    fi
+
+    # IDLE_PROFILE : nginx와 연결되지 않은 profile
+    if [ ${CURRENT_PROFILE} == real1 ]
+    then
+      IDLE_PROFILE=real2
+    else
+      IDLE_PROFILE=real1
+    fi
+
+    # bash script는 값의 반환이 안된다.
+    # echo로 결과 출력 후, 그 값을 잡아서 사용한다.
+    echo "${IDLE_PROFILE}"
+}
+
+# 쉬고 있는 profile의 port 찾기
+function find_idle_port()
+{
+    IDLE_PROFILE=$(find_idle_profile)
+
+    if [ ${IDLE_PROFILE} == real1 ]
+    then
+      echo "8081"
+    else
+      echo "8082"
+    fi
+}
+# !/usr/bin/env bash
+
+# stop.sh
+# 서버 중단을 위한 스크립트
+
+ABSPATH=$(readlink -f $0)
+# ABSDIR : 현재 stop.sh 파일 위치의 경로
+ABSDIR=$(dirname $ABSPATH)
+# import profile.sh
+source ${ABSDIR}/profile.sh
+
+IDLE_PORT=$(find_idle_port)
+
+echo "> $IDLE_PORT 에서 구동중인 애플리케이션 pid 확인"
+IDLE_PID=$(lsof -ti tcp:${IDLE_PORT})
+
+if [ -z ${IDLE_PID} ]
+then
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+  echo "> kill -15 $IDLE_PID"
+  kill -15 ${IDLE_PID}
+  sleep 5
+fi
+#!/usr/bin/env bash
+
+# start.sh
+# 서버 구동을 위한 스크립트
+
+ABSPATH=$(readlink -f $0)
+ABSDIR=$(dirname $ABSPATH)
+source ${ABSDIR}/profile.sh
+
+REPOSITORY=/home/ec2-user/app
+PROJECT_NAME=springboot-intro
 
 echo "> Build 파일 복사"
+echo "> cp $REPOSITORY/deploy/*.jar $REPOSITORY/"
 
-cp $REPOSITORY/zip/*.jar $REPOSITORY/
+cp $REPOSITORY/deloy/*.jar $REPOSITORY/
 
-echo "> 현재 구동 중인 애플리케이션 pid 확인"
-
-CURRENT_PID=$(pgrep -fl spring_deploy_test | grep jar | awk '{print $1}')
-
-echo "현재 구동 중인 애플리케이션 pid : $CURRENT_PID"
-
-if [ -z "$CURRENT_PID" ]; then
-    echo "> 현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
-else
-    echo "> kill -15 $CURRENT_PID"
-    kill -15 $CURRENT_PID
-    sleep 5
-fi
-
-echo "> 새 애플리케이션 배포"
-
+echo "> 새 어플리케이션 배포"
 JAR_NAME=$(ls -tr $REPOSITORY/*.jar | tail -n 1)
 
 echo "> JAR Name: $JAR_NAME"
 
-echo "> JAR_NAME 에 실행권한 추가"
+echo "> $JAR_NAME 에 실행권한 추가"
 
 chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
 
+IDLE_PROFILE=$(find_idle_profile)
+
+echo "> $JAR_NAME 를 profile=$IDLE_PROFILE 로 실행합니다."
 nohup java -jar \
-  -Dspring.config.location=classpath:/application.yml,classpath:/application-real.yml,/home/ec2-user/app/application-oauth.yml,/home/ec2-user/app/application-real-db.yml \
-  -Dspring.profiles.active=real \
-  $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+    -Dspring.config.location=classpath:/application.properties,classpath:/application-$IDLE_PROFILE.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
+    -Dspring.profiles.active=$IDLE_PROFILE \
+    $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
+#!/usr/bin/env bash
+
+# health.sh
+# nginx 연결 설정 변경 전 health-check 용도
+
+ABSPATH=$(readlink -f $0)
+ABSDIR=$(dirname $ABSPATH)
+source ${ABSDIR}/profile.sh
+source ${ABSDIR}/switch.sh
+
+IDLE_PORT=$(find_idle_port)
+
+echo "> Health Check Start!"
+echo "> IDLE_PORT: $IDLE_PORT"
+echo "> curl -s http://localhost:$IDLE_PORT/profile "
+sleep 10
+
+for RETRY_COUNT in {1..10}
+do
+  RESPONSE=$(curl -s http://localhost:${IDLE_PORT}/profile)
+  UP_COUNT=$(echo ${RESPONSE} | grep 'real' | wc -l)
+
+  if [ ${UP_COUNT} -ge 1 ]
+  then # $up_count >= 1 ("real" 문자열이 있는지 검증)
+      echo "> Health check 성공"
+      switch_proxy
+      break
+  else
+      echo "> Health check의 응답을 알 수 없거나 혹은 실행 상태가 아닙니다."
+      echo "> Health check: ${RESPONSE}"
+  fi
+
+  if [ ${RETRY_COUNT} -eq 10 ]
+  then
+    echo "> Health check 실패. "
+    echo "> 엔진엑스에 연결하지 않고 배포를 종료합니다."
+    exit 1
+  fi
+
+  echo "> Health check 연결 실패. 재시도..."
+  sleep 10
+done
+#!/usr/bin/env bash
+
+# switch.sh
+# nginx 연결 설정 스위치
+
+ABSPATH=$(readlink -f $0)
+ABSDIR=$(dirname $ABSPATH)
+source ${ABSDIR}/profile.sh
+
+function switch_proxy() {
+    IDLE_PORT=$(find_idle_port)
+
+    echo "> 전환할 Port: $IDLE_PORT"
+    echo "> Port 전환"
+    # nginx와 연결한 주소 생성
+    # | sudo tee ~ : 앞에서 넘긴 문장을 service-url.inc에 덮어씀
+    echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" | sudo tee /etc/nginx/conf.d/service-url.inc
+
+    echo "> 엔진엑스 Reload"
+    # nignx reload. restart와는 다르게 설정 값만 불러옴
+    sudo service nginx reload
+}
