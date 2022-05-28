3feb9d55b543d1f64e00e8b512823ab05fe5ceb9
langho968 <langho968@gmail.com>
2022-05-28 13:58:57 +0900

    자동배포

--- scripts/switch.sh
+++ scripts/switch.sh
@@ -1,24 +1,29 @@
-echo "> 현재 구동중인  Port 확인"
-CURRENT_PROFILE=$(curl -s http://localhost/profile)
+# switch.sh
 
-if [ $CURRENT_PROFILE == dev ]
-then
-  IDLE_PORT=8082
-elif [ $CURRENT_PROFILE == dev2 ]
-then
-  IDLE_PORT=8081
+#!/bin/bash
+
+# Crawl current connected port of WAS
+CURRENT_PORT=$(cat /home/ec2-user/service_url.inc  | grep -Po '[0-9]+' | tail -1)
+TARGET_PORT=0
+
+echo "> Nginx currently proxies to ${CURRENT_PORT}."
+
+# Toggle port number
+if [ ${CURRENT_PORT} -eq 8081 ]; then
+    TARGET_PORT=8082
+elif [ ${CURRENT_PORT} -eq 8082 ]; then
+    TARGET_PORT=8081
 else
-  echo "> 일치하는 Profile이 없습니다. Profile: $CURRENT_PROFILE"
-  echo "> 8081을 할당합니다."
-  IDLE_PORT=8081
+    echo "> No WAS is connected to nginx"
+    exit 1
 fi
 
-echo "> 전환할 Port: $IDLE_PORT"
-echo "> Port 전환"
-echo "set \$service_url http://127.0.0.1:${IDLE_PORT};" |sudo tee /etc/nginx/conf.d/service-url.inc
+# Change proxying port into target port
+echo "set \$service_url http://127.0.0.1:${TARGET_PORT};" | tee /home/ec2-user/service_url.inc
 
-PROXY_PORT=$(curl -s http://localhost/profile)
-echo "> Nginx Current Proxy Port: $PROXY_PORT"
+echo "> Now Nginx proxies to ${TARGET_PORT}."
 
-echo "> Nginx Reload"
-sudo service nginx reload # reload는 설정만 재적용하기 때문에 바로 적용이 가능합니다.
+# Reload nginx
+sudo service nginx reload
+
+echo "> Nginx reloaded."
