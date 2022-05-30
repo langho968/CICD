575f5bcdd0f03fb9a3f5df466695c9847b4c6582
langho968 <langho968@gmail.com>
2022-05-30 14:26:16 +0900

    컨트롤러 수정

--- src/main/java/com/example/cicdtest/LoggingController.java
+++ src/main/java/com/example/cicdtest/LoggingController.java
@@ -1,16 +1,14 @@
 package com.example.cicdtest;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+
+@RestController
+@RequiredArgsConstructor
 public class LoggingController {
-    @Value("${logging-module.version}")
-    private String version;
-
-    @GetMapping("/")
-    public String version() {
-        return String.format("Project Version : %s", version);
-    }
 
     @GetMapping("/health")
     public String checkHealth() {
